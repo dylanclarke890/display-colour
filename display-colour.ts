4455423fe1ca6250dd744992c718abd5f5586a9c
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-12 08:20:25 +0100

    feat: focus input on click

--- display-colour.ts
+++ display-colour.ts
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector("input") as HTMLInputElement;
   const display = document.querySelector("div#colour-display") as HTMLDivElement;
+  document.body.addEventListener("click", () => input.focus());
   input.addEventListener("keyup", () => {
     let val = input.value;
     if (!val.startsWith("#")) val = `#${val}`;
