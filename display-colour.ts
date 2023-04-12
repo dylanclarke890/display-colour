dd14ad7dfb777fc17027fd060a26826ce2c262c5
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-12 10:24:49 +0100

    feat: added colour picker input to be able to play around with colour

--- display-colour.ts
+++ display-colour.ts
@@ -1,13 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const input = document.querySelector("input") as HTMLInputElement;
+  const input = document.querySelector("#colour") as HTMLInputElement;
+  const colorPicker = document.querySelector("#colour-picker") as HTMLInputElement;
   const display = document.querySelector("div#colour-display") as HTMLDivElement;
   document.body.addEventListener("click", () => input.focus());
   input.addEventListener("keyup", () => {
     let val = input.value;
-    if (!val.startsWith("#") && !val.startsWith("rgb")) val = `#${val}`;
+    if (!val.startsWith("#") && !val.startsWith("rgb")) {
+      val = `#${val}`;
+      input.value = val;
+    }
     display.style.backgroundColor = val;
-  })
+    colorPicker.value = input.value;
+  });
   input.addEventListener("paste", () => {
     input.value = "";
   });
-})
+  colorPicker.addEventListener("change", () => {
+    input.value = colorPicker.value;
+    input.dispatchEvent(new Event("keyup"));
+  });
+});
